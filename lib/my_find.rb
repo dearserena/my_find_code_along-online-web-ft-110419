0ece6a2f9b718a35bfbc0232d45afa953270d8ef
serena Lee <dearserenalee@gmail.com>
2019-11-19 02:20:52 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -6,8 +6,9 @@ i = 0
   if yield(collection[i])
     return 
     collection[i]
+    my_find(collection) {|1| 1 % 3 == 0 and 1 % 5 == 0 }
   end
   i += 1 
   end
-  my_find(collection) {|1| 1 % 3 == 0 and 1 % 5 == 0 }
+  
 end
